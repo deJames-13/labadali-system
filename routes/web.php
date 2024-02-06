6f920faa75f2cadb12a2fbabd5d8059380aaa63c
deJames-13 <de.james013@gmail.com>
2024-02-06 19:14:24 +0800

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('register');
+});
+Route::get('/signin', function () {
+    return view('signin');
+});
+
+Route::get('/user', function () {
+    return view('userdash');
+});
