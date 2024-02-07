31d631becea2b9eba149e5c49bc918352a14e191
deJames-13 <de.james013@gmail.com>
2024-02-07 23:23:52 +0800

    aaadsfasd

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "resources/css/app.css",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
